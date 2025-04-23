95ff66642cd8c2f047e753467f045daf1015b1dd
Mattéo Rannou <matteo.rannouletexier2025@campus-eni.fr>
2025-04-23 16:04:26 +0200

    feat: add CSS file and images for styling the website

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import Banner from './Banner'
+import Cart from './Cart'
 
 function App() {
-    return <Banner />
+    return (<div>
+      <Banner />
+      <Cart />
+      </div>)
 }
 
 export default App
---
+++ src/components/Cart.js
@@ -0,0 +1,24 @@
+function Cart() {
+  const plantes = [
+    { nom: "Monstera", prix: 8 },
+    { nom: "Lierre", prix: 10 },
+    { nom: "Bouquet de fleurs", prix: 15 },
+  ];
+  const total = plantes.reduce((acc, plante) => acc + plante.prix, 0);
+  return (
+    <div>
+      <h2>Mon panier</h2>
+      <ul>
+        {plantes.map((plante, index) => (
+          <li key={index}>
+            {plante.nom} - {plante.prix}€
+          </li>
+        ))}
+      </ul>
+      <p>
+        <strong>Total : {total}€</strong>
+      </p>
+    </div>
+  );
+}
+export default Cart;
